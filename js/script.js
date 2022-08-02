d59a476f754a807a2e735251589665cbd1cd75cd
IDobrBobrI <46893727+IDobrBobrI@users.noreply.github.com>
2022-08-02 21:43:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -72,8 +72,141 @@ function change_colour(click)
 			document.getElementById(parseInt(click)+5).style.backgroundColor = 'gray';
 		}
 		//end top-button
+		check_victory(field);
 }
-(function maksim_generator()
+function check_victory(field) {
+	let sum = 0;
+	for (let i = 0; i <=24; i++) {
+		sum += field[i];
+	}
+	if (sum == 0) {
+		alert("You won!");
+	}
+}
+function gen_1_level() {
+	field = [0, 0, 0, 0, 0,
+			 0, 1, 1, 1, 0,
+			 0, 1, 1, 1, 0,
+			 0, 1, 1, 1, 0,
+			 0, 0, 0, 0, 0]
+	document.getElementById("0").style.backgroundColor = 'gray';
+	document.getElementById("1").style.backgroundColor = 'gray';
+	document.getElementById("2").style.backgroundColor = 'gray';
+	document.getElementById("3").style.backgroundColor = 'gray';
+	document.getElementById("4").style.backgroundColor = 'gray';
+	document.getElementById("5").style.backgroundColor = 'gray';
+
+	document.getElementById("6").style.backgroundColor = 'yellow';
+	document.getElementById("7").style.backgroundColor = 'yellow';
+	document.getElementById("8").style.backgroundColor = 'yellow';
+
+	document.getElementById("9").style.backgroundColor = 'gray';
+	document.getElementById("10").style.backgroundColor = 'gray';
+
+	document.getElementById("11").style.backgroundColor = 'yellow';
+	document.getElementById("12").style.backgroundColor = 'yellow';
+	document.getElementById("13").style.backgroundColor = 'yellow';
+
+	document.getElementById("14").style.backgroundColor = 'gray';
+	document.getElementById("15").style.backgroundColor = 'gray';
+
+	document.getElementById("16").style.backgroundColor = 'yellow';
+	document.getElementById("17").style.backgroundColor = 'yellow';
+	document.getElementById("18").style.backgroundColor = 'yellow';
+
+	document.getElementById("19").style.backgroundColor = 'gray';
+	document.getElementById("20").style.backgroundColor = 'gray';
+	document.getElementById("21").style.backgroundColor = 'gray';
+	document.getElementById("22").style.backgroundColor = 'gray';
+	document.getElementById("23").style.backgroundColor = 'gray';
+	document.getElementById("24").style.backgroundColor = 'gray';
+	return field;
+}
+function gen_2_level() {
+	field = [0, 1, 0, 1, 0,
+			 1, 0, 1, 0, 1,
+			 1, 1, 1, 1, 1,
+			 0, 1, 0, 1, 0,
+			 0, 1, 1, 1, 0]
+	document.getElementById("0").style.backgroundColor = 'gray';
+
+	document.getElementById("1").style.backgroundColor = 'yellow';
+
+	document.getElementById("2").style.backgroundColor = 'gray';
+
+	document.getElementById("3").style.backgroundColor = 'yellow';
+
+	document.getElementById("4").style.backgroundColor = 'gray';
+
+	document.getElementById("5").style.backgroundColor = 'yellow';
+
+	document.getElementById("6").style.backgroundColor = 'gray';
+
+	document.getElementById("7").style.backgroundColor = 'yellow';
+
+	document.getElementById("8").style.backgroundColor = 'gray';
+
+	document.getElementById("9").style.backgroundColor = 'yellow';
+	document.getElementById("10").style.backgroundColor = 'yellow';
+	document.getElementById("11").style.backgroundColor = 'yellow';
+	document.getElementById("12").style.backgroundColor = 'yellow';
+	document.getElementById("13").style.backgroundColor = 'yellow';
+	document.getElementById("14").style.backgroundColor = 'yellow';
+
+	document.getElementById("15").style.backgroundColor = 'gray';
+
+	document.getElementById("16").style.backgroundColor = 'yellow';
+
+	document.getElementById("17").style.backgroundColor = 'gray';
+
+	document.getElementById("18").style.backgroundColor = 'yellow';
+
+	document.getElementById("19").style.backgroundColor = 'gray';
+	document.getElementById("20").style.backgroundColor = 'gray';
+
+	document.getElementById("21").style.backgroundColor = 'yellow';
+	document.getElementById("22").style.backgroundColor = 'yellow';
+	document.getElementById("23").style.backgroundColor = 'yellow';
+
+	document.getElementById("24").style.backgroundColor = 'gray';
+	return field;
+}
+function gen_3_level() {
+	field = [0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 1, 1, 1, 1, 1,
+			 1, 1, 1, 1, 1,
+			 0, 0, 0, 0, 0]
+	document.getElementById("0").style.backgroundColor = 'blue';
+	document.getElementById("1").style.backgroundColor = 'blue';
+	document.getElementById("2").style.backgroundColor = 'blue';
+	document.getElementById("3").style.backgroundColor = 'blue';
+	document.getElementById("4").style.backgroundColor = 'blue';
+	document.getElementById("5").style.backgroundColor = 'blue';
+	document.getElementById("6").style.backgroundColor = 'blue';
+	document.getElementById("7").style.backgroundColor = 'blue';
+	document.getElementById("8").style.backgroundColor = 'blue';
+	document.getElementById("9").style.backgroundColor = 'blue';
+	
+	document.getElementById("10").style.backgroundColor = 'yellow';
+	document.getElementById("11").style.backgroundColor = 'yellow';
+	document.getElementById("12").style.backgroundColor = 'yellow';
+	document.getElementById("13").style.backgroundColor = 'yellow';
+	document.getElementById("14").style.backgroundColor = 'yellow';
+	document.getElementById("15").style.backgroundColor = 'yellow';
+	document.getElementById("16").style.backgroundColor = 'yellow';
+	document.getElementById("17").style.backgroundColor = 'yellow';
+	document.getElementById("18").style.backgroundColor = 'yellow';
+	document.getElementById("19").style.backgroundColor = 'yellow';
+	
+	document.getElementById("20").style.backgroundColor = 'gray';
+	document.getElementById("21").style.backgroundColor = 'gray';
+	document.getElementById("22").style.backgroundColor = 'gray';
+	document.getElementById("23").style.backgroundColor = 'gray';
+	document.getElementById("24").style.backgroundColor = 'gray';
+	return field;
+}
+function gen_4_level()
 {
 	let shuffle = [];
 	for (let i = 0; i <= 24; i++) {
@@ -83,10 +216,9 @@ function change_colour(click)
 	field = shuffle;
 	for (let click = 0; click <= 24; click++) {
 		if (field[click] == 0) {
-			
 		} else {
 			change_colour(click);
 		}
 	}
 	return field;
-}())
+}
